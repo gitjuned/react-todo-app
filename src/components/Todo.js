bae638f24c95077b1ba1332026d33cf13ac8483d
gitjuned <junaydk46@gmail.com>
2020-06-06 12:15:08 +0530

    fix: separate component Todo

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,13 +1,19 @@
 import React from "react";
 import CloseIcon from "../icons/close-icon.svg";
 
-export default function Todo({ item, handleDelete }) {
+export default function Todo({ item, handleDelete, handleCheck }) {
   return (
     <div className="item-wrapper">
-      <span>{item}</span>
+      <input
+        id={item.value}
+        type="checkbox"
+        checked={item.isChecked}
+        onChange={() => handleCheck(item.value)}
+      />
+      <label htmlFor={item.value}>{item.value}</label>
       <button>
         <img
-          onClick={() => handleDelete(item)}
+          onClick={() => handleDelete(item.value)}
           src={CloseIcon}
           alt="close-icon"
         />
